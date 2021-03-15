0b8934b722a5d759f67a3b736ba1526847681746
Felipe Layos <jfelipelayos@gmail.com>
2021-03-15 09:22:59 -0500

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""To_str Module"""
+
+
+def to_str(n: float) -> str:
+    """Get the string representation of a float
+
+    Args:
+        n (float): float to convert
+
+    Returns:
+        str: string representation of the float number
+    """
+
+    return str(n)
