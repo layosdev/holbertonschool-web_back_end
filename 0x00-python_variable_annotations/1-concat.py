253aecbd582a1f390155a40a817cfc065ad85351
Felipe Layos <jfelipelayos@gmail.com>
2021-03-15 09:14:02 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Concat Module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat two strings
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Returns:
+        str: Concatination of two strings
+    """
+
+    return "{}{}".format(str1, str2)
