7700259c67d9fcbe9c6396d001fe33f9352c6e88
Felipe Layos <jfelipelayos@gmail.com>
2021-03-15 08:58:25 -0500

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add Module"""
+
+
+def add(a: float, b: float) -> float:
+    """Gives a sum of two floats
+
+    Args:
+        a (float): First float
+        b (float): Second float
+
+    Returns:
+        float: sum of the two floats
+    """
+
+    return a + b
