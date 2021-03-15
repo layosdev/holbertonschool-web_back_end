936914d0fe1a4810fd4183c2b7f74fecc283e631
Felipe Layos <jfelipelayos@gmail.com>
2021-03-15 09:18:56 -0500

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Floor Module"""
+import math
+
+
+def floor(n: float) -> int:
+    """Calculate the floar of a float
+
+    Args:
+        n (float): Number to calculate
+    """
+
+    return math.floor(n)
