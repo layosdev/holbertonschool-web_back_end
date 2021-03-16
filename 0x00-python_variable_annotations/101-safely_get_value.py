d99a66e9df8e1a53191ddf789edfda4d79e93238
Felipe Layos <jfelipelayos@gmail.com>
2021-03-15 19:41:07 -0500

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -10,6 +10,16 @@ def safely_get_value(dct: Mapping,
                     key: Any,
                     default: Union[T, None] = None
                     ) -> Union[Any, T]:
+    """Get value
+
+    Args:
+        dct (Mapping): [description]
+        key (Any): [description]
+        default (Union[T, None], optional): [description]. Defaults to None.
+
+    Returns:
+        Union[Any, T]: [description]
+    """
     if key in dct:
         return dct[key]
     else:
