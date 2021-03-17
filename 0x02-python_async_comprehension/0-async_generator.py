ce62845f8aba539cf2edbec3b5d6e77401c4d93c
Felipe Layos <jfelipelayos@gmail.com>
2021-03-17 11:05:17 -0500

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-"""Async generator Module"""
+"""Measure runtime Module"""
+
+
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Async Generator
+
+    Yields:
+        Generator[float, None, None]: Generator
+    """
+    for n in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
