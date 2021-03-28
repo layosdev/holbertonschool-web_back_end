df1868ed668508fb0f9c894a6dd49ea81cdd305d
Felipe Layos <jfelipelayos@gmail.com>
2021-03-28 13:12:10 -0500

    Regex-ing

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Filtered logger Module"""
+
+
+from typing import List
+from re import sub
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Filter datum
+
+    Args:
+        fields (List[str]): Fields
+        redaction (str): Redaction
+        message (str): Message
+        separator (str): Separator
+
+    Returns:
+        str: log message obfuscated
+    """
+    for item in fields:
+        message = sub(f'{item}=.+?{separator}',
+                      f'{item}={redaction}{separator}', message)
+    return message
