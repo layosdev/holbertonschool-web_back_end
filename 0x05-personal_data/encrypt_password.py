a1751fccb63131387604097604480e2b24ede5b9
Felipe Layos <jfelipelayos@gmail.com>
2021-03-28 14:50:42 -0500

    Check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -17,3 +17,17 @@ def hash_password(password: str) -> bytes:
     passw = bytes(password, 'utf-8')
     encrypt = bcrypt.hashpw(passw, bcrypt.gensalt())
     return encrypt
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate
+
+    Args:
+        hashed_password (bytes): bytes
+        password (str): password
+
+    Returns:
+        bool: status
+    """
+    status = bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
+    return status
