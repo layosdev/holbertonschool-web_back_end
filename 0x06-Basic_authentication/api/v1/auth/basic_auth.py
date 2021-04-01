02f7ad8b5ab2012df837f37d2c2bb05ee8dce1f4
Felipe Layos <jfelipelayos@gmail.com>
2021-03-31 20:14:49 -0500

    Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,20 @@ class BasicAuth(Auth):
     Args:
         Auth ([type]): Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Base64
+
+        Args:
+            authorization_header (str): Header
+
+        Returns:
+            str: str
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[0:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
