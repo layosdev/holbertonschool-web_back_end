2622380efe214dc4c5dbd94bcd871fe74b97c457
Felipe Layos <jfelipelayos@gmail.com>
2021-03-31 20:43:36 -0500

    Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,3 +51,28 @@ class BasicAuth(Auth):
             return decodification
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """Extract user credentials
+
+        Args:
+            self ([type]): self
+            str ([type]): decoded
+
+        Returns:
+            [type]: str
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        credentials = decoded_base64_authorization_header.split(":")
+
+        return credentials[0], credentials[1]
