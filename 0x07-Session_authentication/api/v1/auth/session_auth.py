e0ff23c33d2534efc076d035ff924fdf5ab3a02a
Felipe Layos <jfelipelayos@gmail.com>
2021-04-06 18:53:34 -0500

    Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Session auth"""
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """SessionAuth
+
+    Args:
+        Auth ([type]): Auth
+    """
+    pass
