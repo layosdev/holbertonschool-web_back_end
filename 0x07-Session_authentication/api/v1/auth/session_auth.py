82a211c0271da2432246db307a1ff5061e4925d3
Felipe Layos <jfelipelayos@gmail.com>
2021-04-06 19:05:38 -0500

    User ID for Session ID

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Session auth"""
 from api.v1.auth.auth import Auth
-from models.user import User
 import uuid
 
 
@@ -11,4 +10,21 @@ class SessionAuth(Auth):
     Args:
         Auth ([type]): Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session
+
+        Args:
+            user_id (str, optional): user_id. Defaults to None.
+
+        Returns:
+            str: str
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
