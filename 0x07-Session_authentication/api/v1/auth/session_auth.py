cbfed369aab684ff7df8fc2f265db882b5e8ca85
Felipe Layos <jfelipelayos@gmail.com>
2021-04-06 19:12:04 -0500

    User ID for Session ID

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,17 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user id for session id
+
+        Args:
+            session_id (str, optional): Session id. Defaults to None.
+
+        Returns:
+            str: string
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
