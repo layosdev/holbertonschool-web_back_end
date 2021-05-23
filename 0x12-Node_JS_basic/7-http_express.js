4216a61f1b239de44292d831db2c20c8f704cef8
Felipe Layos <jfelipelayos@gmail.com>
2021-05-23 12:17:53 -0500

    Create a more complex HTTP server using Express

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const fileData = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${fileData.join('\n')}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
