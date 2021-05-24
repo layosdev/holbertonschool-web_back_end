a44b06420f770085c08b4eedff9781456b40bd33
Felipe Layos <jfelipelayos@gmail.com>
2021-05-24 07:38:34 -0500

    A Building mandatory

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+  }
+
+  // getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  // setters
+
+  // set sqft(sqft) {
+  //   this._sqft = sqft;
+  // }
+
+  evacuationWarningMessage() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}
