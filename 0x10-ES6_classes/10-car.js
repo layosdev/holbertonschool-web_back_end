2612e78da9ad5333d34cadeb75e4c45778d72bcf
Felipe Layos <jfelipelayos@gmail.com>
2021-05-24 08:03:55 -0500

    Vroom

--- 0x10-ES6_classes/10-car.js
+++ 0x10-ES6_classes/10-car.js
@@ -6,7 +6,12 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new this.constructor();
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
