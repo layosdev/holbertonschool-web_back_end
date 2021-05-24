44ee567bbbb2c526c100537048fcf1cf9d9f5cf0
Felipe Layos <jfelipelayos@gmail.com>
2021-05-23 19:39:07 -0500

    A Course, Getters, and Setters

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else throw new TypeError('Name must be a string');
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else throw new TypeError('Length must be a number');
+  }
+
+  set students(students) {
+    if (
+      Array.isArray(
+        students && students.every((student) => typeof student === 'string'),
+      )
+    ) {
+      this._students = students;
+    } else throw new TypeError('Error');
+  }
+}
