89572b7c4bd55bbc44cdbd0fcc9f2378879718c8
Felipe Layos <jfelipelayos@gmail.com>
2021-05-24 07:43:26 -0500

    Inheritance

---
+++ 0x10-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
