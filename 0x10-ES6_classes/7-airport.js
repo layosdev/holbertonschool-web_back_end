1007fc39f4c69489e8b536713c84f41b39817487
Felipe Layos <jfelipelayos@gmail.com>
2021-05-24 07:44:34 -0500

    Airport

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
+export default Airport;
