056bd275dfed83a3e68cc00bdebad88f63d0d6ec
Felipe Layos <jfelipelayos@gmail.com>
2021-09-01 12:06:15 -0500

    We are all unique!

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+)
