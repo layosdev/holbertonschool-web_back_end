35f876835e8e947c71d71f6b4de5d734c9d78735
Felipe Layos <jfelipelayos@gmail.com>
2021-09-02 12:14:32 -0500

    Old school band

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT band_name,
+ifnull(split, 2021)-formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
