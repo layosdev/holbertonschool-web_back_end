5ae67113c06d51d16326c3afecd012953a315169
Felipe Layos <jfelipelayos@gmail.com>
2021-09-02 12:24:50 -0500

    fix year

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
 -- 3 first students in the Batch ID=3
-SELECT band_name,
-	ifnull(split, YEAR(CURDATE())) - formed as lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+-- because Batch 3 is the best!
+SELECT band_name, ifnull(split, 2020) - formed AS lifespan 
+FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
